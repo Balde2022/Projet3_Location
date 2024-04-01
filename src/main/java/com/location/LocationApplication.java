b35a08fd7dc8d4b65ecd32188873ecaa29fcc58d
Balde <abdourahamane620@gmail.com>
2024-04-01 15:14:59 +0200

    Hackage du mot de passe

--- src/main/java/com/location/LocationApplication.java
+++ src/main/java/com/location/LocationApplication.java
@@ -2,12 +2,20 @@ package com.location;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class LocationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LocationApplication.class, args);
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder(){
+		return  new BCryptPasswordEncoder();
 	}
 
 }
--- src/main/java/com/location/Services/User/AuthServiceImpl.java
+++ src/main/java/com/location/Services/User/AuthServiceImpl.java
@@ -2,6 +2,7 @@ package com.location.Services.User;
 
 import com.location.Models.AppUser.User;
 import com.location.Repository.User.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,13 +11,17 @@ import java.util.List;
 @Transactional
 public class AuthServiceImpl implements AuthService{
     private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
 
-    public AuthServiceImpl(UserRepository userRepository) {
+    public AuthServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public User addNewUser(User user) {
+        String password = user.getPassword();
+        user.setPassword(passwordEncoder.encode(password));
         return userRepository.save(user);
     }
 
